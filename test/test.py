90a65b4e4902e9d946796075bbb5b1ac923aac4a
Brenda Bell <b311b-github@theotherbell.com>
2016-12-29 12:35:18 -0500

    Added png conversion to test

--- test/test.py
+++ test/test.py
@@ -1,13 +1,17 @@
 # invocation: python -m test.test
 
 from modules.pcgenerator import PCGenerator
+import cairosvg
 
 pattern='-x--\n-xxx\nxxx-\n--x-\n'
-machine = '12-stitch-br-sr'
+#machine = '12-stitch-br-sr'
 #machine = '24-stitch-br-sr'
-#machine = '40-stitch-deco'
+machine = '40-stitch-deco'
 generator = PCGenerator(None, pattern, machine, 10)
 result = generator.generate()
 text_file = open("{}.svg".format(machine), "w")
 text_file.write(result)
 text_file.close()
+png_file = open("{}.png".format(machine), "w")
+cairosvg.svg2png(bytestring=result,write_to=png_file)
+png_file.close()
