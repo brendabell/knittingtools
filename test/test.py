2d637bd9a478359a1a56f97d8d60cd0376695e5d
Brenda Bell <b311b-github@theotherbell.com>
2017-03-25 22:17:21 -0400

    first crack at 30-stitch knitmaster card

--- test/test.py
+++ test/test.py
@@ -4,11 +4,12 @@ from modules.pcgenerator import PCGenerator
 import cairosvg
 
 #pattern='--x---\n--xxxx\n-xxxx-\nxxxx--\n---x--\n'
-pattern='-x\nx-\n'
+pattern='xxxxxx'
 #machine = '12-stitch-br-sr'
 #machine = '18-stitch-mk70'
 #machine = '24-stitch-br-sr'
-machine = '40-stitch-deco'
+#machine = '40-stitch-deco'
+machine = '30-stitch-km'
 generator = PCGenerator(None, pattern, machine, 10)
 result = generator.generate()
 text_file = open("{}.svg".format(machine), "w")
